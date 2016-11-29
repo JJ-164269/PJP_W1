227a727e9136d34c1dff7e7acd85ed049ebe6908
JJ-164269 <szmati995@gmail.com>
2016-11-29 20:34:17 +0100

    Drawing using 4 squares for each figure
    I changed the part for drawing the figures. Each one consists of 4
    squares.

--- Tetris_w1/Tetris_w1/DrawingCases.h
+++ Tetris_w1/Tetris_w1/DrawingCases.h
@@ -6,28 +6,46 @@ int start_y = 20;
 int c;
 
 void Draw_Case(int c) {
-	if (c == 1)
-		al_draw_filled_rectangle(start_x, start_y, start_x + 64, start_y + 64, al_map_rgb(255, 0, 0));
-	else if (c == 2)
-		al_draw_filled_rectangle(start_x - 96, start_y, start_x - 64, start_y + 128, al_map_rgb(255, 255, 0));
+	if (c == 1) {
+		al_draw_filled_rectangle(start_x, start_y, start_x + 32, start_y + 32, al_map_rgb(255, 0, 0));
+		al_draw_filled_rectangle(start_x + 32, start_y, start_x + 64, start_y + 32, al_map_rgb(255, 0, 0));
+		al_draw_filled_rectangle(start_x, start_y + 32, start_x + 32, start_y + 64, al_map_rgb(255, 0, 0));
+		al_draw_filled_rectangle(start_x + 32, start_y + 32, start_x + 64, start_y + 64, al_map_rgb(255, 0, 0));
+	}
+	else if (c == 2) {
+		al_draw_filled_rectangle(start_x - 96, start_y, start_x - 64, start_y + 32, al_map_rgb(255, 255, 0));
+		al_draw_filled_rectangle(start_x - 96, start_y + 32, start_x - 64, start_y + 64, al_map_rgb(255, 255, 0));
+		al_draw_filled_rectangle(start_x - 96, start_y + 64, start_x - 64, start_y + 96, al_map_rgb(255, 255, 0));
+		al_draw_filled_rectangle(start_x - 96, start_y + 96, start_x - 64, start_y + 128, al_map_rgb(255, 255, 0));
+	}
 	else if (c == 3) {
-		al_draw_filled_rectangle(start_x, start_y + 96, start_x + 64, start_y + 128, al_map_rgb(0, 128, 0));
-		al_draw_filled_rectangle(start_x - 32, start_y + 128, start_x + 32, start_y + 160, al_map_rgb(0, 128, 0));
+		al_draw_filled_rectangle(start_x, start_y + 96, start_x + 32, start_y + 128, al_map_rgb(0, 128, 0));
+		al_draw_filled_rectangle(start_x + 32, start_y + 96, start_x + 64, start_y + 128, al_map_rgb(0, 128, 0));
+		al_draw_filled_rectangle(start_x - 32, start_y + 128, start_x, start_y + 160, al_map_rgb(0, 128, 0));
+		al_draw_filled_rectangle(start_x, start_y + 128, start_x + 32, start_y + 160, al_map_rgb(0, 128, 0));
 	}
 	else if (c == 4) {
-		al_draw_filled_rectangle(start_x, start_y + 192, start_x + 64, start_y + 224, al_map_rgb(0, 0, 128));
-		al_draw_filled_rectangle(start_x + 32, start_y + 224, start_x + 96, start_y + 256, al_map_rgb(0, 0, 128));
+		al_draw_filled_rectangle(start_x, start_y + 192, start_x + 32, start_y + 224, al_map_rgb(0, 0, 128));
+		al_draw_filled_rectangle(start_x + 32, start_y + 192, start_x + 64, start_y + 224, al_map_rgb(0, 0, 128));
+		al_draw_filled_rectangle(start_x + 32, start_y + 224, start_x + 64, start_y + 256, al_map_rgb(0, 0, 128));
+		al_draw_filled_rectangle(start_x + 64, start_y + 224, start_x + 96, start_y + 256, al_map_rgb(0, 0, 128));
 	}
 	else if (c == 5) {
 		al_draw_filled_rectangle(start_x, start_y + 288, start_x + 32, start_y + 320, al_map_rgb(128, 0, 128));
-		al_draw_filled_rectangle(start_x - 32, start_y + 320, start_x + 64, start_y + 352, al_map_rgb(128, 0, 128));
+		al_draw_filled_rectangle(start_x - 32, start_y + 320, start_x, start_y + 352, al_map_rgb(128, 0, 128));
+		al_draw_filled_rectangle(start_x, start_y + 320, start_x + 32, start_y + 352, al_map_rgb(128, 0, 128));
+		al_draw_filled_rectangle(start_x + 32, start_y + 320, start_x + 64, start_y + 352, al_map_rgb(128, 0, 128));
 	}
 	else if (c == 6) {
-		al_draw_filled_rectangle(start_x, start_y + 384, start_x + 96, start_y + 416, al_map_rgb(255, 140, 0));
+		al_draw_filled_rectangle(start_x, start_y + 384, start_x + 32, start_y + 416, al_map_rgb(255, 140, 0));
+		al_draw_filled_rectangle(start_x + 32, start_y + 384, start_x + 64, start_y + 416, al_map_rgb(255, 140, 0));
+		al_draw_filled_rectangle(start_x + 64, start_y + 384, start_x + 96, start_y + 416, al_map_rgb(255, 140, 0));
 		al_draw_filled_rectangle(start_x + 64, start_y + 416, start_x + 96, start_y + 448, al_map_rgb(255, 140, 0));
 	}
 	else if (c == 7) {
-		al_draw_filled_rectangle(start_x, start_y + 480, start_x + 96, start_y + 512, al_map_rgb(199, 21, 133));
+		al_draw_filled_rectangle(start_x, start_y + 480, start_x + 32, start_y + 512, al_map_rgb(199, 21, 133));
+		al_draw_filled_rectangle(start_x + 32, start_y + 480, start_x + 64, start_y + 512, al_map_rgb(199, 21, 133));
+		al_draw_filled_rectangle(start_x + 64, start_y + 480, start_x + 96, start_y + 512, al_map_rgb(199, 21, 133));
 		al_draw_filled_rectangle(start_x, start_y + 512, start_x + 32, start_y + 544, al_map_rgb(199, 21, 133));
 	}
 }
